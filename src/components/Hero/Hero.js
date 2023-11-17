4260b01a4f3297fbe7ff457c9336b2d0dcccc691
M irsyad Ibrahim faqih <144421036+mirsyadibrahimfaqih@users.noreply.github.com>
2023-11-18 01:16:41 +0700

    Add files via upload

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,49 @@
+import React, {useEffect, useState} from 'react';
+import styles from "./Hero.module.css";
+
+const Hero = () => {
+
+    const [movie, setMovie] = useState("")
+    
+    async function getDataApi () {
+        const url = "https://www.omdbapi.com/?apikey=fcf50ae6&i=tt2975590";
+
+        const response = await fetch(url)
+        const data = await response.json()
+
+       setMovie(data)
+    }
+
+    useEffect(
+        function(){
+            getDataApi()
+            console.log(movie)
+        },[]
+    )
+    console.log(movie)
+    
+    return (
+        <div className={styles.container}>
+            <section className={styles.hero}>
+            <div className={styles.hero__left}>
+            <h2 className={styles.hero__title}>{movie.Title}</h2>
+            <h3 className={styles.hero___genre}>
+            {movie.Genre}
+            </h3>
+        <p className={styles.hero__description}>
+        {movie.Plot}
+    </p>
+<button className={styles.hero__button}>Watch</button>
+</div>
+<div className="hero_right">
+<img
+className={styles.hero_image}
+src={movie.Poster}
+alt="placeholder"
+/>
+</div>
+</section>
+</div>
+);
+}
+export default Hero;
